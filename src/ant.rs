94423eebb346fb17f515851bbdf2e775f0dc62bc
Velázquez Cruz Rodrigo Fernando <rodrigo_velazquez99@ciencias.unam.mx>
2020-09-22 18:20:07 -0500

    agregue comentarios

--- src/ant.rs
+++ src/ant.rs
@@ -20,55 +20,44 @@ impl Ant {
 
     // Build a tour from all paths in graph
     pub fn build_tour (&mut self, graph : &Vec::<path::Path>, world : &Vec::<city::City>) {
-        let w = world.clone();
-        println!("CONSTRUYENDO TOUR ----------------------");
+        let world = world.clone();
         let mut actual = unsafe { &*self.nest.clone() };
-        let mut cities_to_find : Vec<city::City> = w.into_iter().filter(|x| *x != actual.clone() ).collect::<Vec<city::City>>().clone();
+        let mut cities_to_find : Vec<city::City> = world.into_iter().filter(|x| *x != actual.clone() ).collect::<Vec<city::City>>().clone();
         while !cities_to_find.is_empty() {
-
+            // Get paths where the actual city is from.
             let mut possible_paths : Vec::<&path::Path> = graph.into_iter().filter(|x| *x.get_from_city() == actual.clone()).collect::<Vec<&path::Path>>().clone();
-            let mut t = self.tour.clone();
-            let searched_cities = t.into_iter().map(|x| unsafe { (*x).get_from_city() }).collect::<Vec<&city::City>>();
-
+            let tour = self.tour.clone();
+            let searched_cities = tour.into_iter().map(|x| unsafe { (*x).get_from_city() }).collect::<Vec<&city::City>>();
+            // Remove paths that have a visited city.
             for path in possible_paths.clone() {
-                let to_city = unsafe { &*path.get_to_city() };
-                let is_in_cities = searched_cities.iter().position(|x| **x == unsafe { (*to_city).clone() });
+                let to_city = path.get_to_city();
+                let is_in_cities = searched_cities.iter().position(|x| **x == (*to_city).clone());
                 match is_in_cities {
                     Some(_) => self.remove_path(&mut possible_paths, &path),
                     _ => continue,
                 }
             }
-
-
-            println!("ciudad actual {} \n", unsafe { (*actual).clone()} );
-            println!("caminos posibles : {}\n", possible_paths.len() );
-            let mut possible_paths_pheromone : f32 = 0.0;
-
             if possible_paths.len() == 0 {
                 break;
             }
-
             // There is only a path avaible
             if possible_paths.len() == 1 {
                 let final_path = possible_paths[0];
                 self.tour.push(final_path);
-                actual = unsafe { &(final_path).get_to_city() };
-                //println!("ciudad final {}", unsafe { (*possible_paths[0]).get_to_city().clone() });
+                actual = &(final_path).get_to_city();
                 cities_to_find.clear();
                 // Path between nest and final city
                 let return_path = graph.iter().find(|x| unsafe { (*x).get_to_city().clone() == (*self.nest).clone() && (*x).get_from_city() == actual}).unwrap();
                 self.tour.push(return_path);
-                println!("\n\n EL TOUR ES: \n\n", );
-                for p in self.tour.clone() {
-                    println!("{}", unsafe { (*p).clone() });
-                }
                 break;
             }
 
-            // Get total probability from possible paths
+            let mut possible_paths_pheromone : f32 = 0.0;
+            // Get total pheromone in possible paths
             for possible_path in &possible_paths {
                 possible_paths_pheromone += possible_path.pheromone;
             }
+
             // Asign each path a probability to choose
             let mut probabilities : Vec<ProbPath> = Vec::new();
             for possible_path in &possible_paths {
@@ -76,33 +65,27 @@ impl Ant {
                 probabilities.push(probability_path);
             }
 
-            // let rand_number = rand::thread_rng().gen_range(1,101);
             let mut rng = rand::thread_rng();
             let rand_number : f32 = rng.gen();
 
-
+            // Select a path with the given probability.
             for probability in probabilities.iter() {
                 if probability.0 <= rand_number {
                     let choosed_path = probability.1;
                     let c = choosed_path as *mut path::Path;
                     self.tour.push (c);
-                    println!("{}", unsafe { (*choosed_path).clone() });
-                    println!("ciudad a la que se dirige {}", unsafe { (*choosed_path).get_to_city() });
                     let index = cities_to_find.iter().position(|x| *x == unsafe { (*choosed_path).get_to_city().clone() } ).unwrap();
                     actual = unsafe { (*choosed_path).get_to_city() };
-                    println!("nueva ciudad actual {}", actual);
                     cities_to_find.remove(index);
-                    let index_path = probabilities.iter().position(|x| unsafe { (*x.1).clone() } == unsafe { (*choosed_path).clone() }).unwrap();
-                    println!("index_path {}", index_path);
-                    println!("{}",unsafe { (*probabilities.remove(index_path).1).clone()});
                     break;
                 }
             }
-            println!("\n ------ Iteracion \n");
         }
-
     }
 
+    /**
+    * Return a clone of every path in tour.
+    **/
     pub fn get_tour (&self) -> Vec::<path::Path> {
         let mut tour : Vec::<path::Path> = Vec::new();
 
@@ -112,30 +95,28 @@ impl Ant {
         return tour;
     }
 
-
+    /**
+    * Remove a path from possible_paths.
+    *
+    * possible_paths : vector of possible paths.
+    * possible_path : path to remove.
+    */
     fn remove_path(&mut self, possible_paths : &mut Vec::<&path::Path>, possible_path : &path::Path) {
         let index = possible_paths.iter().position(|x| **x == *possible_path).unwrap();
         possible_paths.remove(index);
     }
 
+    /**
+    * Return the cost (distance) of tour.
+    */
     pub fn objective_function (&self) -> f32 {
         let mut cost : f32 = 0.0;
         for path in self.tour.clone() {
-            let mut acc = unsafe { (*path).clone() };
+            let acc = unsafe { (*path).clone() };
             cost += acc.euclidean_distance();
         }
         return cost;
     }
-/*
-    pub fn update_paths_tour(&self, q : f32, graph : &mut Vec<path::Path>) {
-        let objective_function = self.objective_function();
-        for path in self.tour.iter() {
-            unsafe {
-                let mut pa = graph.iter().find(|x| **x == **path).unwrap();
-                pa.set_pheromone(pa.pheromone + q / objective_function)
-            }
-        }
-    }*/
 
 }
 
